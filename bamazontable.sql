94c4c753daa49559e5a2e9742b9e7f3c70b63910
Robert Foreman <robazprogrammer@gmail.com>
2019-05-06 15:44:18 -0700

    Add files via upload
    ADDED SQL FILE

---
+++ bamazontable.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(45) NULL,
+    department_name VARCHAR(30) NULL,
+    price INTEGER(20) NOT NULL,
+    stock_quantity INT NOT NULL AUTO_INCREMENT(20),
+    primary key (item_id)
+);
+
+SELECT * FROM products; 
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("gameboy", "electronics", 79.00, 50), 
+	("macbook", "electronics", 799.00, 10),
+	("chromebook", "electronics", 249.00, 25),
+    ("tv", "electronics", 500.00, 50),
+    ("instapot", "cooking", 99.50, 100),
+    ("smoker", "cooking", 249.99, 45),
+    ("blender", "cooking", 35.00, 60),
+    ("sweater", "clothing", 75.00, 25),
+    ("jeans", "clothing", 100.00, 40),
+    ("sneakers", "clothing", 100.00, 35);
+    
+    
+    
+
